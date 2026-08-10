29601ccb062d5186623e1242ee9168ee77df1124
Victor Anih <victoranih76@gmail.com>
2026-08-10 23:29:34 +0100

    last commit

--- settings.js
+++
@@ -1,5 +0,0 @@
-// Toggle dark mode dynamically
-const toggle = document.getElementById("darkToggle");
-toggle.addEventListener("change", () => {
-  document.body.classList.toggle("dark-mode", toggle.checked);
-});
